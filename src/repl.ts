8089bbaff1c164b5c9cacdf0462750aba5a6ff07
ahm4dd <ahmed.emad.edu@gmail.com>
2025-05-24 19:58:25 +0300

    Implemented cleanInput to pass the test

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,12 @@
 export function cleanInput(input: string): string[] {
-  return [];
+  let trimmedAndSplitted: string[] = input.trimEnd().trimStart().split(" ");
+  let result: string[] = [];
+
+  for (const input of trimmedAndSplitted) {
+    if (input === "") {
+      continue;
+    }
+    result.push(input);
+  }
+  return result;
 }
