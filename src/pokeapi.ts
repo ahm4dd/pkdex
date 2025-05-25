c4786968f0e18e20fbc588c87bbdb3224020752e
ahm4dd <ahmed.emad.edu@gmail.com>
2025-05-26 00:43:20 +0300

    Implemented a way to grab locations in PokeAPI

--- src/pokeapi.ts
+++ src/pokeapi.ts
@@ -3,11 +3,29 @@ export class PokeAPI {
 
   constructor() {}
 
-  async fetchLocations(pageURL?: string): Promise<ShallowLocations> {}
+  //async fetchLocations(pageURL?: string): Promise<ShallowLocations> {
 
-  async fetchLocation(locationName: string): Promise<Location> {}
+  //}
+  async fetchLocation(locationName: string): Promise<Location> {
+    const fullURL: string = `${PokeAPI.baseURL}/location-area/${locationName}/`;
+    const response: Response = await fetch(fullURL, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    return await response.json();
+  }
 }
 
-export type ShallowLocations = {};
+export type ShallowLocations = {
+  id: number;
+  name: string;
+};
 
-export type Location = {};
+export type Location = {
+  id: number;
+  name: string;
+  location: ShallowLocations;
+};
