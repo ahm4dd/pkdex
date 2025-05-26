3706ab3fd1edf0373ad6ac0516937bc5561f22cd
ahm4dd <ahmed.emad.edu@gmail.com>
2025-05-26 21:50:57 +0300

    Implemented pokecache for future use

--- src/pokecache.ts
+++ src/pokecache.ts
@@ -5,8 +5,13 @@ export type CacheEntry<T> = {
 
 export class Cache {
   #cache = new Map<string, CacheEntry<any>>();
+  #reapIntervalId: NodeJS.Timeout | undefined = undefined;
+  #interval: number;
 
-  constructor() {}
+  constructor(interval: number) {
+    this.#interval = interval;
+    this.#startReapLoop();
+  }
 
   add<T>(key: string, val: T) {
     this.#cache.set(key, { createdAt: Date.now(), val });
@@ -14,5 +19,22 @@ export class Cache {
 
   get<T>(key: string): CacheEntry<T> | undefined {
     return this.#cache.get(key) ?? undefined;
+  }
+
+  stopReapLoop() {
+    clearInterval(this.#reapIntervalId);
+    this.#reapIntervalId = undefined;
+  }
+
+  #reap() {
+    for (const key in this.#cache.keys()) {
+      if (this.#cache.get(key)!.createdAt < Date.now() - this.#interval) {
+        this.#cache.delete(key);
+      }
+    }
+  }
+
+  #startReapLoop() {
+    this.#reapIntervalId = setInterval(this.#reap, this.#interval);
   }
 }
